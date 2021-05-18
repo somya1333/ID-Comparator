b1297adb17c3e66e6b9768c24fbf908e263f0e8e
Somya Goyal <somyagoyal1333@gmail.com>
2021-05-18 19:07:46 +0530

    Added agent data file

---
+++ agent.py
@@ -0,0 +1,13 @@
+input_data = {
+    'name' : "",
+    'DOB' : "",
+    'PAN' : "",
+    'aadhar' : "",
+    'DL_number' : "",
+    'gaurdian_name' : "",
+    'gender' : "",
+    'mobile' : "",
+}
+
+def get_agent_data():
+    return input_data
