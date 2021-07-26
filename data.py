04363af716d3777dcedf851522f9a6d7dbc05346
Somya Goyal <somyagoyal1333@gmail.com>
2021-07-26 19:44:06 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,23 +8,33 @@ from google.cloud import vision
 credential_path = r'C:\Users\ACER\Desktop\vision api\client.json' 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
+search_data = [
+    name = "NAME",
+    DOB = "DOB",
+    gaurdian_name = "FATHER'S NAME",
+    gender = "GENDER",
+    mobile = "MOBILE",
+]
 
-name = "NAME"
-DOB = "DOB"
-PAN = "PERMANENT ACCOUNT NUMBER CARD"
-aadhar = "AADHAR NO"
-DL_number = "DLNUMBER"
-gaurdian_name = "FATHER'S NAME"
-gender = "GENDER"
-mobile = "MOBILE"
-none = "none"
+search_type = [
+    PAN = "PERMANENT ACCOUNT NUMBER CARD",
+    aadhar = "AADHAR NO",
+    DL_number = "DLNUMBER"
+]
 
 
+# Function to search type of id_card
+def get_type(text_data):
+    for s_type in search_type:
+        if s_type in text_data:
+            return s_type
+    return "none"
+
+# Function to retrieve all data from id_card
 def read_text(image_file):
     
     client_data_list = ["type",
-            "first_name",
-            "last_name",
+            "name",
             "father_name",
             "dob",
             "gender",
@@ -50,73 +60,21 @@ def read_text(image_file):
         # print((text))
         text_data = text.description.upper()
         print((text_data))
-        # agents name
-        if name in text_data:
-            start = text_data.find(name)+len(name)+1
-            end=start
-            while text_data[end]!='\n' and end<len(text_data)-1:
-                end+=1
-            client_data["first_name"] = text_data[start:end]
-            start=end+1
-            while text_data[end]!='\n':
-                end+=1
-            client_data["last_name"] = text_data[start:end]
-        else:
-            client_data["first_name"] = none
-            client_data["last_name"] = none
-
-        # Father's name
-        if gaurdian_name in text_data:
-            start = text_data.find(gaurdian_name)+len(gaurdian_name)+1
-
-            end=start
-            while text_data[end]!='\n' and end<len(text_data):
-                end+=1
-            client_data["father_name"] = text_data[start:end]
-        else:
-            client_data["father_name"] = none
+        
+        for entity in search_data:
             
-        # PAN number
-        if PAN in text_data:
-            start = text_data.find(PAN)+len(PAN)+1
-            end=start
-            while text_data[end]!='\n' and end<len(text_data):
-                end+=1
-            client_data["pan_no"] = text_data[start:end]
-            client_data["type"] = ("PAN CARD")
-        else:
-            client_data["pan_no"] = none
-
-        # DOB
-        if DOB in text_data:
-            start = text_data.find(DOB)+len(DOB)+1
-            end=start
-            while text_data[end]!='\n' and end<len(text_data):
-                end+=1
-            client_data["dob"] = text_data[start:end]
-        else:
-            client_data["dob"] = none
-
-        # Driving License number
-        if DL_number in text_data:
-            start = text_data.find(DL_number)+len(DL_number)+1
-            end=start
-            while text_data[end]!='\n' and end<len(text_data):
-                end+=1
-            client_data["DL_Number"] = text_data[start:end]
-            client_data["type"] = ("DRIVING LICENSE")
-        else:
-            client_data["DL_Number"] = none
-
-        # Aadhar number
-        if aadhar in text_data:
-            start = text_data.find(aadhar)+len(aadhar)+1
-            end=start
-            while text_data[end]!='\n' and end<len(text_data):
-                end+=1
-            client_data["aadhar_number"] = text_data[start:end]
-        else:
-            client_data["aadhar_number"] = none
+            # If data is available in identity card
+            if entity in text_data:
+               start = text_data.find(entity)+len(entity)+1
+                end=start
+                while text_data[end]!='\n' and end<len(text_data):
+                    end+=1
+                client_data[entity] = text_data[start:end]
+            else:
+                client_data[entity] = "none"
+            
+        # Finding the type of id_card
+        client_data["type"] = get_type(text_data)
         break
 
     if response.error.message:
